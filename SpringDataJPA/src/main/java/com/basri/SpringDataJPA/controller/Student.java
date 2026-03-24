1513ca9a6840db528d3c9d1645625ee7e83755dc
Hasan Basri Akcan <77155291+Akcanbasri@users.noreply.github.com>
2026-03-24 07:24:19 +0300

    test

---
+++ SpringDataJPA/src/main/java/com/basri/SpringDataJPA/controller/Student.java
@@ -0,0 +1,22 @@
+package com.basri.SpringDataJPA.controller;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "student")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+    @Column(name = "surname", nullable = false, length = 50)
+    private String surname;
+}
